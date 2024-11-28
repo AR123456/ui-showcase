e22da2b60b2cad0d873ea4c1754c957000257cf1
AR123456 <coloradoz99@gmail.com>
2024-11-28 08:34:17 -0500

    psudo code for cleaning up laser particles

--- Space Shooter 2D/Assets/Scripts/Laser.cs
+++ Space Shooter 2D/Assets/Scripts/Laser.cs
@@ -7,18 +7,20 @@ public class Laser : MonoBehaviour
     // speed variable of 8 
     [SerializeField]
     private float _lzspeed = 8.0f;
-
-    // Start is called before the first frame update
+        // Start is called before the first frame update
     void Start()
-    {
-        
+    {      
     }
-
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
         // laser behavior here - move up infinantly 
         // transform of laser
         transform.Translate(Vector3.up*_lzspeed*Time.deltaTime);
+        // if the laser position of the y coordinate is >= 6.09 it should be removed from the game. 
+        if (true)
+        {
+//
+        }
     }
 }
