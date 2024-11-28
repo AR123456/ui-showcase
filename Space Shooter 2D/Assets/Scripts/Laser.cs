44693ea8b40c41adff602fcb068a26922ae879f6
AR123456 <coloradoz99@gmail.com>
2024-11-28 08:01:57 -0500

    my attempts at moving laser up

--- Space Shooter 2D/Assets/Scripts/Laser.cs
+++ Space Shooter 2D/Assets/Scripts/Laser.cs
@@ -7,13 +7,13 @@ public class Laser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // lasor behavior here - move up infinantly 
-
+        // laser behavior here - move up infinantly 
+        transform.position = new Vector3(0, Mathf.Sin(Time.time), 0);
     }
 }
