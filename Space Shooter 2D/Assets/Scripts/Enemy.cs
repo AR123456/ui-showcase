d4f27e223ec22642dd72537c6d16e0a2636582c2
AR123456 <coloradoz99@gmail.com>
2024-11-30 10:46:06 -0500

    not working my sol

--- Space Shooter 2D/Assets/Scripts/Enemy.cs
+++ Space Shooter 2D/Assets/Scripts/Enemy.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 4f;
+ 
+    private float _randNum = Random.Range(-3, 3);
+
     // Start is called before the first frame update
     void Start()
     {
-       // define starting position of cube 
+        // define starting position of cube Y of 5.73 looks like top of screen
+        transform.position = new Vector3(_randNum, 5.75f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         // move down at rate of 4 meters per second 
-
+        transform.Translate(Vector3.down * (Time.time/_speed));
+        if (transform.position.y <=-15f)
+        {
+            transform.position = new Vector3(0, 5.75f, 0);
+             
+        }
+       
         // when it moves down the full lentgth of the screen, re spawn it at the top with a new random x position - this saves the step of distroing it if it drops off screen
+        // call bottom of screen -15 y 
+
     }
 }
