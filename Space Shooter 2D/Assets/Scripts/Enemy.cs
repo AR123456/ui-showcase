7079cff721c3995b8f7fef9b6fc18b946578b2b2
AR123456 <coloradoz99@gmail.com>
2024-11-30 10:00:58 -0500

    psudocode for enemy

--- Space Shooter 2D/Assets/Scripts/Enemy.cs
+++ Space Shooter 2D/Assets/Scripts/Enemy.cs
@@ -7,12 +7,14 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // define starting position of cube 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move down at rate of 4 meters per second 
+
+        // when it moves down the full lentgth of the screen, re spawn it at the top with a new random x position - this saves the step of distroing it if it drops off screen
     }
 }
