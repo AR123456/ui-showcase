ff2bd4ff784df03d4c046e6058e6bb03b4aa4ff8
AR123456 <coloradoz99@gmail.com>
2024-11-30 11:18:24 -0500

    the correct way to use Random.Range

--- Space Shooter 2D/Assets/Scripts/Enemy.cs
+++ Space Shooter 2D/Assets/Scripts/Enemy.cs
@@ -13,7 +13,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         // define starting position of cube Y of 5.73 looks like top of screen
- 
+        transform.position = new Vector3(0, 5.75f, 0);
     }
 
     // Update is called once per frame
@@ -21,10 +21,12 @@ public class Enemy : MonoBehaviour
     {
         // move down at rate of 4 meters per second - use deltatime, not time 
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-      
-       
-        // when it moves down the full lentgth of the screen, re spawn it at the top with a new random x position - this saves the step of distroing it if it drops off screen
-        // call bottom of screen -15 y 
-
+        //when it moves down the full lentgth of the screen, re spawn it at the top with a new random x position - this saves the step of distroing it if it drops off screen
+        // bottom 0f screen -5f
+        if (transform.position.y <= -5f)
+        {
+            // rand numb between 8 and - 8 - make it a float
+            transform.position = new Vector3(Random.Range(-8f,8f), 5.75f, 0);
+        }
     }
 }
