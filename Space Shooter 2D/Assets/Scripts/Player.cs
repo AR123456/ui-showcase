00dcb0da84f1e51c09c08108744cb331a7b0c323
AR123456 <coloradoz99@gmail.com>
2024-12-01 05:10:53 -0500

    setting up method to track player damage ie lives

--- Space Shooter 2D/Assets/Scripts/Player.cs
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -16,21 +16,23 @@ public class Player : MonoBehaviour
     private float _fireRate = 0.15f;
     [SerializeField]
     private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
     // Start is called before the first frame update
-         void Start()
+    void Start()
     {
         //positon as the start of the game (x,y,z)
-           transform.position = new Vector3(0, 0, 0);
-         }
-        // Update is called once per frame (about 60 frames per second)
+        transform.position = new Vector3(0, 0, 0);
+    }
+    // Update is called once per frame (about 60 frames per second)
     void Update()
     {
-      CalculateMovement();
+        CalculateMovement();
         // when the space bar is pressed, fire laser if cooldown condition met
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire )
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
             FireLaser();
-         }
+        }
     }
     // custom method resposible for all things movement related - call this from update 
     void FireLaser()
@@ -50,7 +52,7 @@ public class Player : MonoBehaviour
         // transform.Translate(new Vector3(1, 0, 0) * 5 * real time);
         transform.Translate(direction * _speed * Time.deltaTime);
         // using clamp for y 
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0),0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
         // x stop on let at -11 , on R if 11 reached reset postion to -11
         if (transform.position.x <= -11)
         {
@@ -59,6 +61,16 @@ public class Player : MonoBehaviour
         else if (transform.position.x >= 11)
         {
             transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+    }
+    public void Damage()
+    {
+        // define the damage trait - here we take damage, thus removing live
+        _lives--;
+        // check if dead
+        if (_lives < 1)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
