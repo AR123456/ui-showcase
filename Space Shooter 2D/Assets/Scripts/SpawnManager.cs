0efb5a596d1db439380d2909ef6561ddec85a929
AR123456 <coloradoz99@gmail.com>
2024-12-02 09:00:25 -0500

    buggy instanciateing

--- Space Shooter 2D/Assets/Scripts/SpawnManager.cs
+++ Space Shooter 2D/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
- 
+    // variable to store the object we want to spawn in the Instantiate method in the IEnumerator
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
@@ -18,11 +21,31 @@ public class SpawnManager : MonoBehaviour
     }
     // spawn game objects every 5 seconds (coroutine)
     // create a coroutine of type IEnumerator 
- 
- 
-      
+ IEnumerator SpawnRoutine()
+    {
+        // iterater for the while loop 
+        int i = 1;
+        // wait 1 frame 
+        yield return null;
+        // then call this 
+     yield return new WaitForSeconds(5.0f);
+        // then do this 
+
         // infinte game loop  while  it will run as long as a contdition is due( beware the infinate loop) 
-            // instantiate object, need ref to it. - the enemy prefab
-                // yeild events  - wait for 5 seconds
-   
+    
+        while (i<5)
+        {
+       //var to randsomize position of _enemyPrefab 
+           Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7f, 0);
+       // instantiate object, need ref to it. - the enemy prefab - put ref to it above start 
+            Instantiate(_enemyPrefab, posToSpawn,Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);         
+        }
+
+       
+    }
+
+
+
+
 }
