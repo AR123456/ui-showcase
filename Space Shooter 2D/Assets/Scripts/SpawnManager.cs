005bfd57ca0737f9b1bc4cbfa97863e3afcd5b07
AR123456 <coloradoz99@gmail.com>
2024-12-02 07:55:51 -0500

    Getting started with SpawnManager coroutine

--- Space Shooter 2D/Assets/Scripts/SpawnManager.cs
+++ Space Shooter 2D/Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // call SpawnRoutine()
     }
 
     // Update is called once per frame
@@ -16,7 +16,11 @@ public class SpawnManager : MonoBehaviour
         
     }
     // spawn game objects every 5 seconds (coroutine)
-    IEnumerator Spawer() {
-    // some code
+    // create a coroutine of type IEnumerator 
+    IEnumerator SpawnRoutine()
+    {
+        // infinte game loop  while  it will run as long as a contdition is due( beware the infinate loop) 
+            // instantiate object, need ref to it. - the enemy prefab
+                // yeild events  - wait for 5 seconds
     }
 }
