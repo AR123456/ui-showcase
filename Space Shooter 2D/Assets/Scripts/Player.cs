29402d52b0c7f326264b4b3d1fb9d5c0d57ea0a9
AR123456 <coloradoz99@gmail.com>
2024-12-05 09:23:44 -0500

    offset for laser is script  and tagging the laser

--- Space Shooter 2D/Assets/Scripts/Player.cs
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -50,7 +50,7 @@ public class Player : MonoBehaviour
         // spawn game object  
         // keep track of this fire for next one
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
     }
     void CalculateMovement()
     {
