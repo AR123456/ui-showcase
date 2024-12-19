cdea4123a4c7c16a08f85e65c6b83e5a9bb508ac
AR123456 <coloradoz99@gmail.com>
2024-12-19 09:02:55 -0500

    psudocode score and text update

--- Space Shooter 2D/Assets/Scripts/UIManager.cs
+++ Space Shooter 2D/Assets/Scripts/UIManager.cs
@@ -14,6 +14,8 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         // assign text componet to handle
+     
+        _scoreText = GetComponent<Text>();
     }
 
     // Update is called once per frame
