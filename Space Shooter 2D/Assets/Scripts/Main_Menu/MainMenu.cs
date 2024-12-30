a4ba9883e9e3368b422729389bab177c60e64ad2
AR123456 <coloradoz99@gmail.com>
2024-12-30 08:33:07 -0500

    method in script run with button click

--- Space Shooter 2D/Assets/Scripts/Main_Menu/MainMenu.cs
+++ Space Shooter 2D/Assets/Scripts/Main_Menu/MainMenu.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ // method to call with button click 
+ public void LoadGame() { }
 }
