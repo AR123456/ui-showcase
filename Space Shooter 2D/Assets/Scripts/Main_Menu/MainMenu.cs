e05c1b14133bbaec57a6bbf6f37581558d209293
AR123456 <coloradoz99@gmail.com>
2024-12-30 08:34:34 -0500

    add sceneManagement name space and loadScene in load game method

--- Space Shooter 2D/Assets/Scripts/Main_Menu/MainMenu.cs
+++ Space Shooter 2D/Assets/Scripts/Main_Menu/MainMenu.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// bring in the SceneManagement name space
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
  // method to call with button click 
  public void LoadGame() {
-    // load game scene - build settings 
+        // load game scene - build settings position 2 in array index 1
+        SceneManager.LoadScene(1); 
     }
 }
