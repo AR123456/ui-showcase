88dd1bbcba68bc2d49fd7262ff519df92c2632c7
AR123456 <coloradoz99@gmail.com>
2025-01-04 08:17:29 -0500

    using transform rotate to rotate asteroid

--- Space Shooter 2D/Assets/Scripts/Asteroid.cs
+++ Space Shooter 2D/Assets/Scripts/Asteroid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    private float _rotateSpeed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +14,8 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // rotate object on z continuously 
-        // going to need to do something with transform.position
+        transform.Rotate(Vector3.forward* _rotateSpeed * Time.deltaTime);
    
     }
-    void MoveAsteroid()
-    {
-        //x= whatever it current position is
-        // y = whatever its curren position is 
-        // x = increment up float up 1 * by a speed * Time.deltaTime
-        // no change to x or y now z will change 
-        //transform.Translate(0, 0, 0);
-     
-    }
+    
 }
