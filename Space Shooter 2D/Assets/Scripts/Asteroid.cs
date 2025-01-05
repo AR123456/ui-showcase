e19567b1f0fe1cedf7ab808514691923ebeacace
AR123456 <coloradoz99@gmail.com>
2025-01-05 06:42:50 -0500

    psudocode for explosion asteroid script

--- Space Shooter 2D/Assets/Scripts/Asteroid.cs
+++ Space Shooter 2D/Assets/Scripts/Asteroid.cs
@@ -7,18 +7,14 @@ public class Asteroid : MonoBehaviour
     [SerializeField]
     private float _rotateSpeed = 19.0f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
         // pass in the axis to rotate
         transform.Rotate(Vector3.forward* _rotateSpeed * Time.deltaTime);
-  
-
-    }
-    
+      }
+    // check for Laser colllission (trigger)
+    // instantiate explosion at postion of the astorid (us)
 }
