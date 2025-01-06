2a6e03f0853d1ab0ab090c85641294ce27f01f9a
AR123456 <coloradoz99@gmail.com>
2025-01-06 05:34:13 -0500

    coded out no errors but not exploding

--- Space Shooter 2D/Assets/Scripts/Asteroid.cs
+++ Space Shooter 2D/Assets/Scripts/Asteroid.cs
@@ -6,8 +6,10 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed = 19.0f;
-  
-
+        [SerializeField]
+   private bool _isExplosionActive=false;
+    [SerializeField]
+    private GameObject _laserPrefab;
     // Update is called once per frame
     void Update()
     {
@@ -17,13 +19,22 @@ public class Asteroid : MonoBehaviour
     // check for Laser collission (trigger)
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.tag == "Laser")
+        if (_laserPrefab!=null)
         {
             //trigger explosion animation 
-            _anim.SetTrigger("OnLaserHitAsteroid");
+           ExplosionActive();
             
         }
     }
 
     // instantiate explosion at postion of the astorid (us)
+     public void ExplosionActive(){
+     _isExplosionActive=true;
+        StartCoroutine(OnLaserHitAsteroidPowerDownRoutine());
+      }
+    // clean up after 3 sec 
+      IEnumerator OnLaserHitAsteroidPowerDownRoutine(){
+      yield return new WaitForSeconds(3.0f);
+        _isExplosionActive = false;
+      }
 }
