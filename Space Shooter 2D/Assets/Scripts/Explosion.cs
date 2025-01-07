8bfcdfbec92b21a9a8329e6adfd99421211110a6
AR123456 <coloradoz99@gmail.com>
2025-01-07 06:52:57 -0500

    call destroy method with slight delay in explosion script

---
+++ Space Shooter 2D/Assets/Scripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this will be called when the Explosion starts so just destroy
+        Destroy(this.gameObject, 3f);
+    }
+
+   
+}
