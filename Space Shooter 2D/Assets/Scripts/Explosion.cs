c63ce431cadd2d8ca2ee1b1a433105863f953756
AR123456 <coloradoz99@gmail.com>
2025-01-15 07:38:22 -0500

    working clip on asteroid

--- Space Shooter 2D/Assets/Scripts/Explosion.cs
+++ Space Shooter 2D/Assets/Scripts/Explosion.cs
@@ -8,6 +8,7 @@ public class Explosion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      
         // this will be called when the Explosion starts so just destroy
         Destroy(this.gameObject, 3f);
                
