17e670a11950e008a9003ad7cae2ce518af1c4b5
AR123456 <coloradoz99@gmail.com>
2025-01-18 07:09:04 -0500

    getting rid of extra comments

--- Space Shooter 2D/Assets/Scripts/PowerUp.cs
+++ Space Shooter 2D/Assets/Scripts/PowerUp.cs
@@ -6,31 +6,22 @@ public class PowerUp : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.0f;
- 
-    // need to identify which power up using intiger system- give ids
-    //ID for Powerups 0=tripleshot, 1=speed,2=Shields
+     //ID for Powerups 0=tripleshot, 1=speed,2=Shields
     [SerializeField]
-    // initializing to set in inspector - will be avalible in prefabs
     private int powerupID;
-    // ref to powerup sound - serialize to apply in inspector
     [SerializeField]
     private AudioClip _clip;
- 
-    // Update is called once per frame
+     // Update is called once per frame
     void Update()
     {
-        // move down at rate of 3 meters per second - use deltatime, not time 
+        // move down 3 meters per second 
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        // bottom 0f screen -5f
-        if (transform.position.y <= -4.5f)
+       if (transform.position.y <= -4.5f)
         {
             Destroy(this.gameObject);
         }
     }
-    // detect collision with player
-    // only to be collectable by the player- tags
-    // on collect destroy
-    private void OnTriggerEnter2D(Collider2D other)
+        private void OnTriggerEnter2D(Collider2D other)
     {
        if(other.tag == "Player")
         {
@@ -58,7 +49,7 @@ public class PowerUp : MonoBehaviour
                 }
 
             }
-            // destroy the power up 
+            // destroy  power up 
             Destroy(this.gameObject);
         } 
     }
